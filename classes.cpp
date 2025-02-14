12ced29e98917c445ea66990a3941363f08a5362
theotimeqrt <theotimequiret@gmail.com>
2025-02-14 15:59:37 +0100

    collab

--- classes.cpp
+++ classes.cpp
@@ -3,8 +3,24 @@
 
 using namespace std;
 
+class Jeu_ping_pong {
+    public:
+        int number;
+        void sayHello() {
+            cout << "Hello, my number is " << number << endl;
+        }
+};
 
+class table {
+    public:
+        int largeur = 183;
+        int longueur = 274;
+        void sayHello() {
+            cout << "Hello, my taille is " << largeur << ","<< longueur << endl;
+        }
+};
 
 int main() {
     return 0;
 }
+
